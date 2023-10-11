cbb4bb8725536ad0b20563aecb9fdd3600550008
Bran Zanger <66757687+branzanger@users.noreply.github.com>
2023-10-11 14:43:35 -0700

    Add files via upload

---
+++ Lab2/NumberServer.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Number: %d", num);
+        } else if (url.getPath().equals("/increment")) {
+            num += 1;
+            return String.format("Number incremented!");
+        } else {
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("count")) {
+                    num += Integer.parseInt(parameters[1]);
+                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class NumberServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
---
+++ Lab2/README.md
@@ -0,0 +1,8 @@
+To run the server:
+```bash
+javac Server.java Program.java
+java Main [port]
+//e.g: java Main 8080
+```
+
+“`wavelet`” – noun; a small wave of water; a ripple (Oxford Dictionary). A cute name for a little web server from UCSD (Joe)
